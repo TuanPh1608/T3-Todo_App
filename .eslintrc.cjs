9e8dbbf09c18f1b2830a24896c768af5784e7386
PHUNG TUAN <ptuan160804@gmail.com>
2025-04-04 22:37:07 +0700

    Fix for deploy

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,49 +1,10 @@
-
-import { FlatCompat } from "@eslint/eslintrc";
-import tseslint from "typescript-eslint";
+const { FlatCompat } = require("@eslint/eslintrc");
+const path = require("path");
 
 const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
+  baseDirectory: __dirname, // Chỉ rõ thư mục gốc cho ESLint
 });
 
-export default tseslint.config(
-  {
-    ignores: [".next"],
-  },
-  ...compat.extends("next/core-web-vitals"),
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    extends: [
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
-    rules: {
-      "@typescript-eslint/array-type": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/consistent-type-imports": [
-        "warn",
-        { prefer: "type-imports", fixStyle: "inline-type-imports" },
-      ],
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        { checksVoidReturn: { attributes: false } },
-      ],
-    },
-  },
-  {
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-      },
-    },
-  },
-);
+module.exports = {
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+};
